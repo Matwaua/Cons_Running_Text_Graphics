10a4241b21a0b5bfebdef8dcacdad913f11a8378
matwaua <matheusferreirawa@gmail.com>
2024-10-15 18:08:31 -0400

    The Start, nothing interesting yet

--- src/myself/code/main_graphics/GamePanel.java
+++ src/myself/code/main_graphics/GamePanel.java
@@ -2,22 +2,29 @@ package myself.code.main_graphics;
 
 import myself.code.inputs.KeyHandler;
 
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageOutputStream;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class GamePanel extends JPanel implements Runnable {
-    final int originalTileSize = 16;
-    final int scale = 3;
-    public final int tileSize = originalTileSize * scale;
-    public final int maxScreenCol = 16;
-    public final int maxScreenRow = 12;
-    public final int screenWidth = tileSize * maxScreenCol;
-    public final int screenHeight = tileSize * maxScreenRow;
+    public static final int CHAR_SCALE = 10;
+
+    public static final int CHAR_WIDTH_RAW = 5;
+    public static final int CHAR_HEIGHT_RAW = 6;
+
+    public static final int CHAR_WIDTH= CHAR_WIDTH_RAW * CHAR_SCALE;
+    public static final int CHAR_HEIGHT= CHAR_HEIGHT_RAW * CHAR_SCALE;
+
+    public static final int SCREEN_WIDTH = 768;
+    public static final int SCREEN_HEIGHT = 576;
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
 
     public GamePanel () {
-        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
         this.addKeyListener(keyH);
@@ -57,8 +64,10 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void paintComponent (Graphics g) {
         super.paintComponent(g);
-
+        char[][] arrayOfChars = {{'A', 'A', 'A'}, {98, 100, 98}, {'/', '|', '\\'}};
+        LettersDecoder decoder = new LettersDecoder(13, 8, "src/myself/resources/letters/letters.png");
         Graphics2D g2 = (Graphics2D)g;
+        decoder.printAllChars(arrayOfChars, 5, 5, g2);
         g2.dispose();
     }
 }
--- src/myself/code/main_graphics/LettersDecoder.java
+++ src/myself/code/main_graphics/LettersDecoder.java
@@ -1,4 +1,41 @@
 package myself.code.main_graphics;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static myself.code.main_graphics.GamePanel.*;
+
 public class LettersDecoder {
+    private BufferedImage[] allCharSprites;
+    public LettersDecoder (int charsPerRow, int charsPerColum, String pathToCharSprites) {
+        allCharSprites = new BufferedImage[charsPerColum * charsPerRow];
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File(pathToCharSprites));
+        } catch (IOException e) {
+            System.err.println("could not find the file: " + e);
+            return;
+        }
+
+        for (int i = 0; i < charsPerColum; i++) {
+            for (int j = 0; j < charsPerRow; j++) {
+                allCharSprites[i*charsPerRow + j] = image.getSubimage(j * CHAR_WIDTH_RAW, i * CHAR_HEIGHT_RAW, CHAR_WIDTH_RAW, CHAR_HEIGHT_RAW);
+            }
+        }
+    }
+
+    public BufferedImage getCharSprite (char wantedChar) {
+        return allCharSprites[wantedChar - 33];
+    }
+
+    public void printAllChars (char[][] charsToPrint, int x, int y, Graphics2D g2) {
+        for (int i = 0; i < charsToPrint.length; i++) {
+            for (int j = 0; j < charsToPrint[i].length; j++) {
+                g2.drawImage(getCharSprite(charsToPrint[i][j]), x + (j * CHAR_WIDTH), y + (i * CHAR_HEIGHT), CHAR_WIDTH, CHAR_HEIGHT, null);
+            }
+        }
+    }
 }
